5f1dd2fb4a6b15fb998b9103085517c15a05dd98
Sanketdave12 <sddave1998@gmail.com>
2021-05-12 15:22:14 +0530

    Checkout-Done

--- static/js/script.js
+++ static/js/script.js
@@ -1,47 +1,52 @@
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
-  $('.carousel').owlCarousel({
-      stagePadding: 0,
-      margin: -150,
-      loop:true,
-      autoplay:true,
-      autoplayTimeout:3000,
-      autoplayHoverPause: true,
-      responsive: {
-        0: {
-          items: 3,
-        },
-        600: {
-          items: 2,
-        },
-        1000: {
-          items: 4,
-        },
-      },
+//   $('.carousel').owlCarousel({
+//       stagePadding: 0,
+//       margin: -150,
+//       loop:true,
+//       autoplay:true,
+//       autoplayTimeout:3000,
+//       autoplayHoverPause: true,
+//       responsive: {
+//         0: {
+//           items: 3,
+//         },
+//         600: {
+//           items: 2,
+//         },
+//         1000: {
+//           items: 4,
+//         },
+//       },
 
-  });
-  $('.carousel1').owlCarousel({
-      stagePadding: 0,
-      margin:10,
-      loop:true,
-      autoplay:true,
-      autoplayTimeout:2000,
-      autoplayHoverPause: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        600: {
-          items: 2,
-        },
-        1000: {
-          items: 4,
-        },
-      },
-  });
+//   });
+//   $('.carousel1').owlCarousel({
+//       stagePadding: 0,
+//       margin:10,
+//       loop:true,
+//       autoplay:true,
+//       autoplayTimeout:2000,
+//       autoplayHoverPause: true,
+//       responsive: {
+//         0: {
+//           items: 1,
+//         },
+//         600: {
+//           items: 2,
+//         },
+//         1000: {
+//           items: 4,
+//         },
+//       },
+//   });
   
-});
+// });
 
 
+console.log("before")
+document.querySelector(".online-mode[type='radio']").addEventListener('click',function(){
+  console.log("after")
+  document.querySelector(".text").textContent = "Pay At Desk";
+})
